1d5b4658661489b3819c217eae9c92127a7228e9
anish.m.prasad <anish.m.prasad@gmail.com>
2018-11-01 00:44:45 +0530

    SliderItem update

---
+++ src/components/SliderItem.js
@@ -0,0 +1,36 @@
+// C.r("4e", function (e, s, t) {
+//   "use strict";
+//   var i = (e("dP"), e("fE")),
+//     r = e("aP"),
+//     a = e("dm"),
+//     d = e("9p"),
+//     n = r({
+//       displayName: "SliderItem",
+
+//       statics: {
+//         className: "slider-item"
+//       },
+
+//     });
+//   s.exports = n
+// });
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class SliderItem extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    var e = a.extend({
+      "slider-item": !0
+    }, this.props.additionalClasses);
+    return 0 !== this.props.viewportIndex && (e["slider-item-" + this.props.viewportIndex] = !0), React.createElement("div", {
+      className: 'slider-item'
+    }, this.props.children)
+  }
+}
+SliderItem.defaultProps = {
+  additionalClasses: {}
+}
