eec8ce6237d9050eb2bf34d91d2a807a81cd8b81
anish.m.prasad <anish.m.prasad@gmail.com>
2019-03-13 12:31:18 +0530

    update

--- src/components/TitleCardContainer.js
+++ src/components/TitleCardContainer.js
@@ -79,48 +79,48 @@ export default class TitleCardContainer extends Component {
 			this.onBoxartFocusEnd && (this.onBoxartFocusEnd(), delete this.onBoxartFocusEnd),
 			(this._isMounted = !1);
 	}
-	getBoxart(e, t, o) {
-		return this.context.listContext === listContexts.LIST_CONTEXTS.SHORT_FORM
-			? e.getValueSync(['interestingMoment', t, o])
-			: e.getValueSync(['boxarts', t, o]);
-	}
-	getFalcorVideoModel() {
-		return this.props.videoRoot ? this.props.model.bind([this.props.videoRoot]) : this.props.model;
-	}
-	getVideoModel() {
-		var e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : this.state,
-			t = this.getFalcorVideoModel(),
-			o = b.getExtensionForOpaqueImage(this.context.models),
-			i = this.context.listContext === listContexts.LIST_CONTEXTS.SHORT_FORM,
-			n = i ? 50 : 160,
-			a = {
-				bob: e.isBobOpen,
-				shortForm: i,
-				videoMerch: this.context.getModelData('truths').hasVideoMerchInBob,
-				tallPanel: this.props.isTallPanel
-			},
-			r = {
-				opaqueExtension: o,
-				standardImageSize: J,
-				highQualityImageSize: h.SIZE,
-				maxSynopsisLength: n,
-				tallVMEnabled: this.context.getModelData('truths').hasROAR30SecondPreview
-			},
-			d = x(t, a, r);
-		if (k.get('web.ui.akira.enable.maturity.mismatch.logging')) {
-			var c = s.get(this.context.models, 'userInfo.data.maturity', 1e6),
-				u = s.get(t.getValueSync(['titleMaturity']), 'level', 0);
-			c < u && ((d.maturityMisMatchEdgy = !0), u > 96 && (d.maturityMisMatchNonEdgy = !0));
-		}
-		return d;
-	}
+	// getBoxart(e, t, o) {
+	// 	return this.context.listContext === listContexts.LIST_CONTEXTS.SHORT_FORM
+	// 		? e.getValueSync(['interestingMoment', t, o])
+	// 		: e.getValueSync(['boxarts', t, o]);
+	// }
+	// getFalcorVideoModel() {
+	// 	return this.props.videoRoot ? this.props.model.bind([this.props.videoRoot]) : this.props.model;
+	// }
+	// getVideoModel() {
+	// 	var e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : this.state,
+	// 		t = this.getFalcorVideoModel(),
+	// 		o = b.getExtensionForOpaqueImage(this.context.models),
+	// 		i = this.context.listContext === listContexts.LIST_CONTEXTS.SHORT_FORM,
+	// 		n = i ? 50 : 160,
+	// 		a = {
+	// 			bob: e.isBobOpen,
+	// 			shortForm: i,
+	// 			videoMerch: this.context.getModelData('truths').hasVideoMerchInBob,
+	// 			tallPanel: this.props.isTallPanel
+	// 		},
+	// 		r = {
+	// 			opaqueExtension: o,
+	// 			standardImageSize: J,
+	// 			highQualityImageSize: h.SIZE,
+	// 			maxSynopsisLength: n,
+	// 			tallVMEnabled: this.context.getModelData('truths').hasROAR30SecondPreview
+	// 		},
+	// 		d = x(t, a, r);
+	// 	if (k.get('web.ui.akira.enable.maturity.mismatch.logging')) {
+	// 		var c = s.get(this.context.models, 'userInfo.data.maturity', 1e6),
+	// 			u = s.get(t.getValueSync(['titleMaturity']), 'level', 0);
+	// 		c < u && ((d.maturityMisMatchEdgy = !0), u > 96 && (d.maturityMisMatchNonEdgy = !0));
+	// 	}
+	// 	return d;
+	// }
 	getBobTrackingInfo = () => {
-		var e = this.videoModel,
-			t = e.userRating,
-			o = e.userRatingRequestId,
-			i = t.matchScore,
-			n = t.tooNewForMatchScore,
-			s = t.predicted;
+		// var e = this.videoModel,
+		// 	t = e.userRating,
+		// 	o = e.userRatingRequestId,
+		// 	i = t.matchScore,
+		// 	n = t.tooNewForMatchScore,
+		// 	s = t.predicted;
 		return getTrackingInfoFromContext;
 		// return this.props.getTrackingInfoFromContext(
 		// 	{
