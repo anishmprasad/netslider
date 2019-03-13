e45709a8891f681eae40e6cd06ed62e7fe92616e
anish.m.prasad <anish.m.prasad@gmail.com>
2019-03-13 17:10:36 +0530

    code cleanup

---
+++ build/precache-manifest.82432c33e921e6b002f383af68dfeb57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c45844e380bc4bd8be4",
+    "url": "/static/css/main.0bc85c9f.chunk.css"
+  },
+  {
+    "revision": "3c45844e380bc4bd8be4",
+    "url": "/static/js/main.3c45844e.chunk.js"
+  },
+  {
+    "revision": "36110cf723f8068b8091",
+    "url": "/static/js/1.36110cf7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d06af404b99a3c030d2d9030d99f783d",
+    "url": "/index.html"
+  }
+];
