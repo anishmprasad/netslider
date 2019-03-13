064fcd29b8cbd6fb18070ffdf195ac461baf7d40
anish.m.prasad <anish.m.prasad@gmail.com>
2019-03-13 18:21:51 +0530

    big fix

--- src/lib/PresTrackedContainer.js
+++ src/lib/PresTrackedContainer.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 
 export default function PresTrackedContainer(props) {
 	return(
-		<div className= {'ptrack-container' + this.props.className}>
-			{this.props.children}
+		<div className= {'ptrack-container' + props.className}>
+			{props.children}
 		</div>
 	)
 }
