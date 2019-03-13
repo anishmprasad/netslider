29c59cfc6861145f1cfe709b12b4f9e303f17057
anish.m.prasad <anish.m.prasad@gmail.com>
2019-03-13 18:12:04 +0530

    code cleanup

--- src/lib/BoxArtContainer.js
+++ src/lib/BoxArtContainer.js
@@ -3,29 +3,44 @@
 import React from 'react';
 import classnames from 'classnames';
 export default function BoxArtContainer(props) {
-	var a = props.className,
-		t = props.title,
-		n = props.children;
-	return React.createElement(
-		'div',
-		{
-			className: classnames(a, 'boxart-container')
-		},
-		n,
-		t &&
-			React.createElement(
-				'div',
-				{
-					className: 'fallback-text-container',
-					'aria-hidden': !0
-				},
-				React.createElement(
-					'div',
-					{
-						className: 'fallback-text'
-					},
-					t
-				)
-			)
-	);
+	// var a = props.className,
+	// 	t = props.title,
+	// 	n = props.children;
+	// return React.createElement(
+	// 	'div',
+	// 	{
+	// 		className: classnames(a, 'boxart-container')
+	// 	},
+	// 	n,
+	// 	t &&
+	// 		React.createElement(
+	// 			'div',
+	// 			{
+	// 				className: 'fallback-text-container',
+	// 				'aria-hidden': !0
+	// 			},
+	// 			React.createElement(
+	// 				'div',
+	// 				{
+	// 					className: 'fallback-text'
+	// 				},
+	// 				t
+	// 			)
+	// 		)
+	// );
+	return(
+		<div className={classnames(props.className, 'boxart-container')}>
+			{props.children}
+			{props.title && 
+				<div
+					className={'fallback-text-container'}
+					aria-hidden={!0}
+				>
+					<div className='fallback-text'>
+						{props.title}
+					</div>
+				</div>
+			}
+		</div>
+	)
 }
--- src/lib/PresTrackedContainer.js
+++ src/lib/PresTrackedContainer.js
@@ -2,14 +2,10 @@
 
 import React, { Component } from 'react';
 
-export default class PresTrackedContainer extends Component {
-	render() {
-		return React.createElement(
-			'div',
-			{
-				className: 'ptrack-container' + this.props.className
-			},
-			this.props.children
-		);
-	}
+export default function PresTrackedContainer(props) {
+	return(
+		<div className= {'ptrack-container' + this.props.className}>
+			{this.props.children}
+		</div>
+	)
 }
