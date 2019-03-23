49471cc05d1537c2ef4d776e8cb908e26bc58957
anish.m.prasad <anish.m.prasad@gmail.com>
2019-03-23 13:39:43 +0530

    code refactor

--- src/lib/BobCardContainer.js
+++ src/lib/BobCardContainer.js
@@ -73,22 +73,23 @@ export default class BobCardContainer extends Component {
 		);
 	}
 	render() {
-		var e = this.props,
-			o = this.props.playbackQueryParams,
-			s = this.props.onBobOpen,
-			t = this.props.videoModel,
-			n = this.props.onBobClose,
-			i = this.props.isBobOpen,
-			r = this.props.isTallPanel,
-			h = this.props.enableMetaDataHiding,
-			d = this.props.videoModel.hasVideoMerch,
+		var 
+			// e = this.props,
+			// o = this.props.playbackQueryParams,
+			// s = this.props.onBobOpen,
+			// t = this.props.videoModel,
+			// n = this.props.onBobClose,
+			// i = this.props.isBobOpen,
+			// r = this.props.isTallPanel,
+			// h = this.props.enableMetaDataHiding,
+			// d = this.props.videoModel.hasVideoMerch,
 			g = this.getBobToggles();
 		return(
 			<BobCard
 				videoModel={this.props.videoModel}
 				model={ this.props.model}
-				titleCardImage={ t.image}
-				titleCardImageHighRes={ t.imageHighRes}
+				titleCardImage={ this.props.videoModel.image}
+				titleCardImageHighRes={ this.props.videoModel.imageHighRes}
 				playbackQueryParams={ this.props.playbackQueryParams}
 				isBobOpen={ this.props.isBobOpen}
 				showMetaData={ g.showMetaData}
@@ -109,30 +110,6 @@ export default class BobCardContainer extends Component {
 				isMutedByDefault={ false}
 			/>
 		)
-		// return React.createElement(BobCard, {
-		// 	videoModel: t,
-		// 	model: this.props.model,
-		// 	titleCardImage: t.image,
-		// 	titleCardImageHighRes: t.imageHighRes,
-		// 	playbackQueryParams: o,
-		// 	isBobOpen: i,
-		// 	showMetaData: g.showMetaData,
-		// 	showThumbRatingIcons: g.showThumbRatingIcons,
-		// 	showEvidence: g.showEvidence,
-		// 	showAddToMyList: g.showAddToMyList,
-		// 	showAudioToggle: g.showAudioToggle,
-		// 	showRecentInterestingMoment: g.showRecentInterestingMoment,
-		// 	showProgressBar: g.showProgressBar,
-		// 	showProgressSummary: g.showProgressSummary,
-		// 	showNewEpisodeBadge: g.showNewEpisodeBadge,
-		// 	showPlayButton: g.showPlayButton,
-		// 	enableMetaDataHiding: h,
-		// 	onMylistChange: this.onMylistChange,
-		// 	onBobOpen: s,
-		// 	onBobClose: n,
-		// 	onClickJawHitZone: this.onClickJawHitZone,
-		// 	isMutedByDefault: false
-		// });
 	}
 }
 
