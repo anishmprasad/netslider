e9023e08ab4a5643c7f7d9750b4eddc87bcf4088
anish.m.prasad <anish.m.prasad@gmail.com>
2019-03-24 22:56:52 +0530

    code cleanup

--- src/lib/PresTrackedContainer.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-
-import React, { Component } from 'react';
-
-export default function PresTrackedContainer(props) {
-	return <div className={'ptrack-container' + props.className}>{props.children}</div>;
-}
